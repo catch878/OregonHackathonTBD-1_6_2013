fec5bc1dd0308bfe0c06fbd4398773498def2a3c
farzonl <farzonl@gmail.com>
2013-06-01 12:54:45 -0400

    Maps and boiler plate objects

---
+++ Android/src/com/hack/monkeybuisness/LocationActivity.java
@@ -0,0 +1,14 @@
+package com.hack.monkeybuisness;
+
+import com.google.android.maps.MapActivity;
+
+public class LocationActivity extends MapActivity 
+{
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ Android/src/com/hack/monkeybuisness/data/CompanyProfile.java
@@ -0,0 +1,16 @@
+package com.hack.monkeybuisness.data;
+
+public class CompanyProfile {
+	
+	String Name;
+	String Desc;
+	String Address;
+	public CompanyProfile(String n, String d, String a)
+	{
+		this.Name    = n;
+		this.Desc    = d;
+		this.Address = a;
+	}
+
+
+}
